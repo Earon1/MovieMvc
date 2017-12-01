e30293785f2c7e593201394650c0f01814f1895b
Earon1 <arongaspic1@gmail.com>
2017-12-01 16:25:11 +0100

    Third page done
    Created databases, handles adding, deleting, editing, viewing details...

---
+++ MovieMvc/MovieMvc/Data/MovieMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieMvc.Models
+{
+    public class MovieMvcContext : DbContext
+    {
+        public MovieMvcContext (DbContextOptions<MovieMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieMvc.Models.Movie> Movie { get; set; }
+    }
+}
